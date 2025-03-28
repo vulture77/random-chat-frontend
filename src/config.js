26a8d74a9ecbf8865667f0aeb8c9eec3abf2f55f
sivaprasad510 <60322348+vulture77@users.noreply.github.com>
2025-03-28 22:59:43 +0530

    Delta2

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 // export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
-export const API_URL = "https://yourbackend.vercel.app";
+// export const API_URL = "https://yourbackend.vercel.app";
+export const API_URL = "http://192.168.1.100:5000";
